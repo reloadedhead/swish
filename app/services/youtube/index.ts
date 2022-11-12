db701ab083ff145d699eca36b207b4cade6c0d27
Tomás García Gobet <tomasgobet@icloud.com>
2022-11-12 15:23:39 +0100

    adds youtube service

---
+++ app/services/youtube/index.ts
@@ -0,0 +1,28 @@
+import { PlaylistItemsResponse } from "./types";
+
+class YouTube {
+  private key: string;
+
+  constructor(key: string) {
+    this.key = key;
+  }
+
+  /**
+   * Retrieves a {@link https://developers.google.com/youtube/v3/docs/playlistItems#resource-representation playlist items} object by id.
+   * @param playlistId the id of the playlist items to retrieve.
+   * @returns playlist items.
+   */
+  public async getPlaylistItems(
+    playlistId: string
+  ): Promise<PlaylistItemsResponse> {
+    const url = new URL("https://www.googleapis.com/youtube/v3/playlistItems");
+    url.searchParams.set("part", "snippet");
+    url.searchParams.set("maxResults", "200");
+    url.searchParams.set("playlistId", playlistId);
+    url.searchParams.set("key", this.key);
+
+    return await (await fetch(url)).json();
+  }
+}
+
+export default new YouTube(process.env.YOUTUBE_API_KEY!);
---
+++ app/services/youtube/types.ts
@@ -0,0 +1,49 @@
+export type PlaylistItemResponse = {
+  kind: "youtube#playlistItem";
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      medium: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      high: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    channelTitle: string;
+    playlistId: string;
+    position: number;
+    resourceId: {
+      kind: "youtube#video";
+      videoId: string;
+    };
+  };
+  videoOwnerChannelTitle: string;
+  videoOwnerChannelId: string;
+};
+
+export type PlaylistItemsResponse = {
+  kind: "youtube#playlistItemListResponse";
+  etag: string;
+  nextPageToken: string;
+  prevPageToken: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: PlaylistItemResponse[];
+};
