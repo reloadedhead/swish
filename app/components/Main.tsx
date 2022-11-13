26c70e9993ac05f44bc5aa26c6b6fa416c366ec0
Tomás García Gobet <tomasgobet@icloud.com>
2022-11-13 20:22:12 +0100

    updates styles to main component

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from "react";
 
 export default function Main({ children }: PropsWithChildren) {
-  return <main className="p-2">{children}</main>;
+  return (
+    <main className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">{children}</main>
+  );
 }
